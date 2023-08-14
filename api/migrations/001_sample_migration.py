10989456fcee96d12fdc2170cd4a994264d1e443
Roland <cook.rolandcook.roland@gmail.com>
2023-08-14 13:59:45 -0400

    organization

--- api/migrations/001_sample_migration.py
+++ api/migrations/001_sample_migration.py
@@ -2,37 +2,18 @@ steps = [
     [
         # "Up" SQL statement
         """
-        CREATE TABLE dummy (
+        CREATE TABLE reservation (
             id SERIAL PRIMARY KEY NOT NULL,
-            required_limited_text VARCHAR(1000) NOT NULL,
-            required_unlimited_text TEXT NOT NULL,
-            required_date_time TIMESTAMP NOT NULL,
-            automatically_set_date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            required_integer INTEGER NOT NULL,
-            required_money MONEY NOT NULL
+            first_name VARCHAR(100) NOT NULL,
+            last_name VARCHAR(100) NOT NULL,
+            email VARCHAR(100) NOT NULL,
+            jump_date DATE NOT NULL,
+            jumper_text TEXT 
         );
         """,
         # "Down" SQL statement
         """
-        DROP TABLE dummy;
+        DROP TABLE reservation;
         """
     ],
-    [
-        # "Up" SQL statement
-        """
-        CREATE TABLE big_dummy (
-            id SERIAL PRIMARY KEY NOT NULL,
-            required_limited_text VARCHAR(1000) NOT NULL,
-            required_unlimited_text TEXT NOT NULL,
-            required_date_time TIMESTAMP NOT NULL,
-            automatically_set_date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            required_integer INTEGER NOT NULL,
-            required_money MONEY NOT NULL
-        );
-        """,
-        # "Down" SQL statement
-        """
-        DROP TABLE big_dummy;
-        """
-    ]
 ]
---
+++ api/queries/reservations.py
@@ -0,0 +1,49 @@
+from pydantic import BaseModel
+from typing import Optional
+from queries.pool import pool
+from datetime import date,time
+
+class Jumper(BaseModel):
+    name:str
+
+class ReservationIn(BaseModel):
+    first_name: str
+    last_name: str
+    phone_number: str
+    email: str
+    jumper_type: Jumper
+    date: date
+    time: time
+
+class ReservartionOut(BaseModel):
+    id:int
+    first_name: str
+    last_name: str
+    phone_number: str
+    email: str
+    jumper_type: Jumper
+    date: date
+    time: time
+
+class Reservationrepository:
+    def create(self, reservation: ReservationIn) -> ReservartionOut:
+        # connect to database
+            with pool.connection() as conn:
+            # get cursor
+                with conn.cursor() as db:
+                # run insert statment
+                  result =  db.execute(
+                         """
+                        INSERT INTO reservations
+                            (first_name, last_name, phone_number, email, jumper_type, date, time)
+                        VALUES 
+                            (%s,%s,%s,%s,%s,%s);
+                            RETURNING id;
+                        """,
+                        [reservation.first_name, reservation.last_name, reservation.phone_number, reservation.email, reservation.jumper_type, reservation.date, reservation.time]
+                    )
+                id = result.fetchone()[0]
+                data = reservation.dict()
+                return ReservartionOut(id=id, **data)
+                
+                #return new data

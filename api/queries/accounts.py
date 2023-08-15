ab05fc836d874b997fbdc12376ab2fbad51886ec
PedroAgustinDejesus <dejesuspedro07@gmail.com>
2023-08-15 16:36:32 -0400

    added account querie get

--- api/queries/accounts.py
+++ api/queries/accounts.py
@@ -24,24 +24,41 @@ class AccountOutWithPassword(AccountOut):
 
 
 class AccountQueries:
-    def get(self, email: str) -> AccountOut:
-        with pool.connection() as conn:
-            with conn.cursor() as db:
-                result = db.execute(
-                    """
-                    Select id,
-                    first_name,
-                    last_name,
-                    hashed_password,
-                    email,
-                    username
-                From account
-                Where email = %s
-                    """,
-                    [email]
-                )
-                record = result.fetchone()[0]
-                
+    try:
+        def get(self, email: str) -> AccountOut:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        Select id,
+                        first_name,
+                        last_name,
+                        hashed_password,
+                        email,
+                        username
+                    From account
+                    WHERE email = %s
+                        """,
+                        [email]
+                    )
+                    record = result.fetchone()[0]
+                    if record is None:
+                        return None
+                    account = AccountOutWithPassword(
+                            id=record[0],
+                            email=record[1],
+                            first_name=record[2],
+                            last_name=record[3],
+                            username=record[4]
+                        )
+                    return account
+    except Exception as e:
+        print(e)
+        
+
+
+
+
 
 
     def create(self, account: AccountIn, hashed_password: str) -> AccountOut:

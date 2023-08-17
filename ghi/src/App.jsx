9120ff9077347b37a76f0042976e4a53b0c7a953
Derrick <dlamdlam3@yahoo.com>
2023-08-17 14:07:23 -0400

    NavBar and mainpage

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -1,21 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Form from './Form'
+import React, { useState } from "react";
+import "./index.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Form from "./Form";
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentPage, setCurrentPage] = useState("home"); // Default to "home"
 
   return (
-    <>
-      <div>
-        <Form/>
-      </div>
+    <div className="app-container">
+      <Navbar setCurrentPage={setCurrentPage} />
+      <main className="main-content">
+        {currentPage === "home" && <Home />}
+        {currentPage === "form" && <Form />}
+      </main>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ ghi/src/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Website</h1>
+      <p>This is the homepage of my website.</p>
+      <p>Feel free to explore and learn more about us.</p>
+    </div>
+  );
+}
+
+export default Home;
